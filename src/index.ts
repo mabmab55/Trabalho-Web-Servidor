7f90b2b790f0ce43e18496614be916f9c7119268
mabmab55 <bambam_lacerda001@hotmail.com>
2023-09-05 21:54:17 -0300

    chore: configure express ts handlebars

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+import { engine } from "express-handlebars";
+
+const app = express();
+
+app.use(express.json());
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => {
+	res.render("home");
+});
+
+
+app.listen(3333, () => "server running on port 3333");
