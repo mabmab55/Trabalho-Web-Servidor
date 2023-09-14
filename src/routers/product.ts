be20ab55540e9eb2eb0b5641caa3b56866aa776a
Luis Otavio <luisotavioduarterodrigues@hotmail.com>
2023-09-13 22:45:10 -0300

    feat: finish product page

--- src/routers/product.ts
+++ src/routers/product.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-
-
+import { products } from "../db";
 export const product = Router();
 
-product.get("/product", (req, res) => {
-    res.render("product");
-});
+product.get("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const product = products.find((product) => product.id === id);
+    console.log(product);
 
+    if (product) {
+        res.render("product", { product });
+        return;
+    }
+    return res.render("product", { error: "Tênis não encontrado!" });
+});
